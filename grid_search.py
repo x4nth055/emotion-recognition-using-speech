f272d7afaefb1f99d920096d6d8c3a89393254b6
x4nth055 <fullclip@protonmail.com>
2020-09-02 18:10:57 +0200

    updated best parameters for grid search

--- grid_search.py
+++ grid_search.py
@@ -12,7 +12,10 @@ import pickle
 from emotion_recognition import EmotionRecognizer
 from parameters import classification_grid_parameters, regression_grid_parameters
 
+# emotion classes you want to perform grid search on
 emotions = ['sad', 'neutral', 'happy']
+# number of parallel jobs during the grid search
+n_jobs = 4
 
 best_estimators = []
 
@@ -23,7 +26,7 @@ for model, params in classification_grid_parameters.items():
         params['n_neighbors'] = [len(emotions)]
     d = EmotionRecognizer(model, emotions=emotions)
     d.load_data()
-    best_estimator, best_params, cv_best_score = d.grid_search(params=params)
+    best_estimator, best_params, cv_best_score = d.grid_search(params=params, n_jobs=n_jobs)
     best_estimators.append((best_estimator, best_params, cv_best_score))
     print(f"{emotions} {best_estimator.__class__.__name__} achieved {cv_best_score:.3f} cross validation accuracy score!")
 
@@ -39,7 +42,7 @@ for model, params in regression_grid_parameters.items():
         params['n_neighbors'] = [len(emotions)]
     d = EmotionRecognizer(model, emotions=emotions, classification=False)
     d.load_data()
-    best_estimator, best_params, cv_best_score = d.grid_search(params=params)
+    best_estimator, best_params, cv_best_score = d.grid_search(params=params, n_jobs=n_jobs)
     best_estimators.append((best_estimator, best_params, cv_best_score))
     print(f"{emotions} {best_estimator.__class__.__name__} achieved {cv_best_score:.3f} cross validation MAE score!")
 
